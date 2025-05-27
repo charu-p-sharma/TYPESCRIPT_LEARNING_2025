e9d2cdd86819ba372c728581b2fb081c949c1b53
charu <charupsharma5@gmail.com>
2025-05-27 22:45:39 +0530

    string, number, boolean primitive variables

--- Variable/variable.js
+++ Variable/variable.js
@@ -1,6 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// STRING
 var greet = "charu";
 console.log(greet);
-greet = true;
+// the below code will give an error in TS, as i have defined greet as string
+// greet = true
 console.log(greet);
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable
+// Example: greet.
+// NUMBER
+var userId = 2341; // It is same as 2341.3 ,it is still a number only 
+console.log(userId);
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable of type number
+// Example: userId.
+// BOOLEAN 
+var isAdult = true;
+console.log(isAdult);
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable of type boolean
+// Example: userId.
+// Note: The below is not a good practice as it is overuse of TS
+var age;
+age = 23;
+console.log("age", age);
--- Variable/variable.ts
+++ Variable/variable.ts
@@ -1,12 +1,39 @@
+// STRING
 let greet:string="charu";
 console.log(greet);
 
 // the below code will give an error in TS, as i have defined greet as string
-greet = true
+// greet = true
 console.log(greet);
 
-// Note: If you do variablename and dot it will give lists of all the methods that can be used with this varible
-// greet.
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable
+// Example: greet.
+
+
+
+
+// NUMBER
+let userId=2341  // It is same as 2341.3 ,it is still a number only 
+console.log(userId);
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable of type number
+// Example: userId.
+
+
+
+
+// BOOLEAN 
+let isAdult:boolean=true
+console.log(isAdult);
+// Note: If you do variablename and dot it will give lists of all the methods that can be used with this variable of type boolean
+// Example: userId.
+
+
+
+// Note: The below is not a good practice as it is overuse of TS
+let age:number
+age = 23;
+console.log("age",age);
+
 
 
 
