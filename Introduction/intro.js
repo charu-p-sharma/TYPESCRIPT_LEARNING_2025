4cb3f3a4c2efd518a0c1a3d8926a7969d48c6209
charu <charupsharma5@gmail.com>
2025-05-27 22:19:05 +0530

    intro to typescript

---
+++ Introduction/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "charu", age: 23 };
+console.log("Charu");
+console.log(user.name);
+console.log(user.email);
---
+++ Introduction/intro.ts
@@ -0,0 +1,4 @@
+let user={name:"charu",age:23}
+console.log("Charu");
+console.log(user.name);
+console.log(user.email);  //this is an error in TypeScript becasuse email doesnt exists
